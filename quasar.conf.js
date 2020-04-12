8acdb998e3ee56c1889da5e08a44334390eaaf79
Sixl-Daniel <sixl.daniel@googlemail.com>
2020-04-12 17:39:09 +0200

    Change theme-color of PWA

--- quasar.conf.js
+++ quasar.conf.js
@@ -113,7 +113,7 @@ module.exports = function (ctx) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#3a7599',
+        theme_color: '#405258',
         icons: [{
             src: 'statics/icons/icon-128x128.png',
             sizes: '128x128',
