c873239164c8cf1d7e33c36183ec46058ba4df33
Sixl-Daniel <sixl.daniel@googlemail.com>
2020-04-29 16:35:06 +0200

    Move drawer to component, fix mailto-link

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,35 +19,7 @@
             </q-toolbar>
         </q-header>
 
-        <q-drawer v-model="rightDrawerOpen" side="right">
-            <q-scroll-area>
-                <div class="q-pa-md q-gutter-sm text-center">
-                    <SocialLink v-for="link in linksSocial" :key="link.link" v-bind="link" />
-                </div>
-
-                <q-list>
-                    <q-item-label header>Menü</q-item-label>
-                    <MainMenuLink v-for="link in linksInternal" :key="link.title" v-bind="link" />
-
-                    <q-item-label header>Externe Links</q-item-label>
-                    <MainMenuLink v-for="link in linksExternal" :key="link.title" v-bind="link" />
-
-                    <q-item-label header>Kontakt</q-item-label>
-                    <MainMenuLink v-for="link in linksContact" :key="link.title" v-bind="link" />
-                </q-list>
-            </q-scroll-area>
-            <q-img
-                id="profile-image-bg"
-                class="absolute-top"
-                src="/statics/img/profile/profile-bg.jpg"
-                draggable="false"
-            >
-                <div class="absolute-bottom bg-transparent text-right">
-                    <div class="text-weight-bold">Daniel Sixl</div>
-                    <div class="text-weight-thin">Web Development</div>
-                </div>
-            </q-img>
-        </q-drawer>
+        <drawer :open="rightDrawerOpen" />
 
         <q-page-container>
             <transition
@@ -72,15 +44,13 @@
 </template>
 
 <script>
-import MainMenuLink from "components/MainMenuLink";
-import SocialLink from "components/SocialLink";
+import Drawer from "components/Drawer";
 
 export default {
     name: "MainLayout",
 
     components: {
-        MainMenuLink,
-        SocialLink
+        Drawer
     },
 
     computed: {
@@ -93,78 +63,7 @@ export default {
 
     data() {
         return {
-            rightDrawerOpen: false,
-            linksInternal: [
-                {
-                    title: "Startseite",
-                    icon: "fas fa-home",
-                    link: "/"
-                },
-                {
-                    title: "Datenschutz",
-                    icon: "fas fa-shield-alt",
-                    link: "/datenschutz"
-                },
-                {
-                    title: "Impressum",
-                    icon: "fas fa-id-badge",
-                    link: "/impressum"
-                }
-            ],
-            linksExternal: [
-                {
-                    title: "Private Cloud",
-                    caption: "cloud.sixl.org",
-                    icon: "fas fa-cloud",
-                    link: "https://cloud.sixl.org/",
-                    target: "_blank"
-                },
-                {
-                    title: "Poi-Kurs",
-                    caption: "poi-kurs.de",
-                    icon: "fas fa-fire",
-                    link: "https://poi-kurs.de/",
-                    target: "_blank"
-                }
-            ],
-            linksContact: [
-                {
-                    title: "Telefon",
-                    caption: "+49 911 92370377",
-                    icon: "fas fa-phone",
-                    link: "tel:+4991192370377"
-                },
-                {
-                    title: "Mobil",
-                    caption: "+49 176 56896000",
-                    icon: "fas fa-mobile-alt",
-                    link: "tel:+4917656896000"
-                },
-                {
-                    title: "E-Mail",
-                    caption: "sixl@sixl.org",
-                    icon: "fas fa-at",
-                    link: "sixl@sixl.org"
-                }
-            ],
-            linksSocial: [
-                {
-                    icon: "fab fa-linkedin",
-                    link: "https://www.linkedin.com/in/daniel-sixl/"
-                },
-                {
-                    icon: "fab fa-github",
-                    link: "https://github.com/Sixl-Daniel/"
-                },
-                {
-                    icon: "fab fa-stack-overflow",
-                    link: "https://stackoverflow.com/users/story/7233016"
-                },
-                {
-                    icon: "fab fa-codepen",
-                    link: "https://codepen.io/Sixl/"
-                }
-            ]
+            rightDrawerOpen: false
         };
     }
 };
